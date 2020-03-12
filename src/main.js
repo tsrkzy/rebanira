802c16cf03adafb4ed3ac492ca88b768e8fe6ca4
tsrkzy <tsrmix@gmail.com>
2020-03-12 19:43:28 +0900

    add firebase hosting configure

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import {initFirebase} from "./firebase/firebase";
+initFirebase();
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount("#app");
