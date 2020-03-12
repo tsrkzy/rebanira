cc0027b1f911e9f9944147df4dabf862e89ca1b2
tsrkzy <tsrmix@gmail.com>
2020-03-12 22:53:11 +0900

    vuetify

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import vuetify from './plugins/vuetify'
+
 Vue.config.productionTip = false;
 
 import {initFirebase} from "./firebase/firebase";
 initFirebase();
 
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount("#app");
